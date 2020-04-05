779d3c2ac75518b5376e08375a164f537950414a
Haadi <Hadiyoville25@gmail.com>
2020-04-05 22:27:10 +0500

    ML data processing template updated

--- GD.py
+++ GD.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
 
 # Reading file and seperating dependent and independent variables.
 dataset = pd.read_excel("irisdataset.xlsx")
@@ -18,7 +19,6 @@ X_train,X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_s
 
 # random weight vector
 weight = np.random.rand(4, 1)
-
 
 
 
